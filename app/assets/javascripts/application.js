b9977c4b64e6222217726780539f8b0f585e2de0
Jack <jack@jwhapps.co>
2016-10-28 12:11:12 +1300

    heroku fix

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,8 +11,8 @@
 // about supported directives.
 //
 //= require jquery
-//= require jquery_ujs
 //= require twitter/bootstrap
+//= require jquery_ujs
 // EQUALS HERE OR NO TURBO LINKS require turbolinks
 //= require_tree .
 	
